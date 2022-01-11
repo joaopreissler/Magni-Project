86aeabf454624355bc24ee86f8d1cc19d502ed58
João Victor Preissler Marcolino Alberto <joaovictorpreissler@hotmail.com>
2022-01-11 00:09:44 -0300

    Fix bugs

--- MagniProject.bat
+++ MagniProject.bat
@@ -1,15 +1,19 @@
 echo off
+WHERE dotnet ef
+IF %ERRORLEVEL% NEQ 0 START /WAIT powershell -Command "dotnet tool install --global dotnet-ef --version 3.0.0"
 START /WAIT powershell -Command "SqlLocalDb.exe create "MagniProject" -s"
 START /WAIT powershell -Command "SqlLocalDB.exe start "MagniProject"" 
+xcopy "%~dp0\data\MagniProject.mdf" "%HOMEDRIVE%%HOMEPATH%"  
+xcopy "%~dp0\data\MagniProject_log.ldf" "%HOMEDRIVE%%HOMEPATH%" 
+EXIT /B
 cd  %~dp0\BackEnd\MagniFinance\MagniFinance
-START /WAIT powershell -Command "dotnet ef Database update" 
 START /WAIT powershell -Command "dotnet ef Database update" 
 cd ..
 cd .. 
 cd ..
 cd %~dp0\frontend
 if exist client-win32-x64 goto yesfile
-if not exist c:\test\file.txt goto nofile
+if not exist client-win32-x64 goto nofile
 goto end
 :yesfile
 cd client-win32-x64
